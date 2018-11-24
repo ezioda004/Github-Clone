08599f9915bde4e1602694c17f71b08997133e8c
Aditya Verma <adi004.zz@gmail.com>
2018-11-24 19:27:50 +0530

    feat: added profile search functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,42 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import Profile from "./Components/Profile/Profile";
 class App extends Component {
+  state = {
+    name: "",
+    found: false
+  }
+  onSubmitHandler = e => {
+    e.preventDefault();
+    fetch(`https://api.github.com/users/${this.state.name}`)
+      .then(data => data.json())
+      .then(data => {
+        console.log(data)
+        if (data.message !== "Not Found"){
+          window.location.href = "/profile"
+        }
+      });
+  }
+  onChangeHandler = e => {
+    this.setState({
+      name: e.target.value
+    });
+  }
   render() {
     return (
       <div>
-        <Route path="/" exact render={() => <div>Hello world</div>} />
-        <Route path = "/profile" exact  component = {Profile}/>
-        test;
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <div>
+              <form onSubmit = {this.onSubmitHandler}>
+                <input  value = {this.state.name} onChange = {this.onChangeHandler}/>
+                <button type = "submit"> Submit </button>
+              </form>
+            </div>
+          )}
+        />
+        <Route path="/profile" exact component={Profile} />
       </div>
     );
   }
