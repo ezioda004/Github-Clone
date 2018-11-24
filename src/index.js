82b9bafa7c93d118ba29c51892cbb12b7c6b2af3
Aditya Verma <adi004.zz@gmail.com>
2018-11-24 19:12:38 +0530

    feat: inital redux setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/index";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store = {store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 
---
+++ src/reducers/index.js
@@ -0,0 +1,3 @@
+export const reducer = (state = 0, action) => {
+    return state++;
+}
